31adc4b698a3cafc99fd4a5229f48fa82c6f1469
Bernhard Spindler <bernhard.spindler@unicredit.at>
2017-02-21 16:23:58 +0100

    making async testcase work (not asyc right now :) )

--- src/test/TestJniPingAsync.java
+++ src/test/TestJniPingAsync.java
@@ -5,26 +5,31 @@ import static org.junit.Assert.*;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import at.spindi.WinPingResult;
 
 public class TestJniPingAsync {
 
+	private class TestResult<T> {
+		public T result;
+	}
+	
 	@Test
 	public void testSimpleAsync() throws UnknownHostException {
 		
-		WinPingResult res = null;
-		final Object waitObj = new Object();
-		boolean success = false;
+		TestResult<WinPingResult> res = new TestResult<>();
 		
 		at.spindi.WinPing.ping4Async(
 				(Inet4Address)Inet4Address.getByName("127.0.0.1"),
 				1000,
 				(result) -> {
-					System.out.printf("callback!");
+					//System.out.printf("callback!");
+					res.result = result;
 				});
-		
+		Assert.assertNotNull(res.result);
+		Assert.assertEquals(123, res.result.RoundTripTime);	
 	}
 
 }
