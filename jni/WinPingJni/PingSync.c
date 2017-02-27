f1e85faa22225908cd25947382988e6dc120f6a9
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2017-02-27 11:21:36 +0100

    implemented Startup/Cleanup. Renamed files

--- jni/WinPingJni/PingSync.c
+++ jni/WinPingJni/PingSync.c
@@ -2,7 +2,7 @@
 #define VC_EXTRALEAN
 
 #include <windows.h>
-#include <IPHlpApi.h>
+//#include <IPHlpApi.h>
 #include <IcmpAPI.h>
 
 // Need to link with Iplhlapi.lib
@@ -17,37 +17,30 @@
 #endif 
 
 BOOLEAN WINAPI 
-DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID    Reserved)
+DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
 {
 	BOOLEAN bSuccess = TRUE;
 	return bSuccess;
 }
 
-//#pragma intrinsic (memset)
+extern HANDLE g_hIcmpFile;
 
 JNIEXPORT jint JNICALL 
 Java_at_spindi_WinPing_native_1icmp_1WinPing4 (JNIEnv *env, jclass cl, jint IpAdress, jint TimeoutMs) {
 		
-	const HANDLE hIcmpFile = IcmpCreateFile();
-	if (hIcmpFile == INVALID_HANDLE_VALUE) {
-		return GetLastError();
-	}
-
 	MY_DATA			SendData = { .data = "WinPingJni Send Buffer Data" };
-	MY_ICMP_REPLY	ReplyBuffer = { 0 };
+	MY_ICMP_REPLY	ReplyBuffer;// = { 0 };
 
+#ifdef _DEBUG	
 	int sizeSendData	= sizeof(SendData);
 	int sizeReplyBuffer = sizeof(ReplyBuffer);
 	int sizeIcmpReply	= sizeof(ReplyBuffer.reply);
-
-
-#ifdef _DEBUG
 	DBGPRINT(L"sizeof(ReplyBuffer) = %d\n", sizeReplyBuffer);
 	DBGPRINT(L"sizeof(IcmpReply)   = %d\n", sizeIcmpReply);
 #endif
 
 	DWORD ReplysReceived = IcmpSendEcho(
-		hIcmpFile,
+		g_hIcmpFile,
 		IpAdress,
 		(LPVOID)&SendData,
 		sizeof(SendData),
@@ -57,21 +50,16 @@ Java_at_spindi_WinPing_native_1icmp_1WinPing4 (JNIEnv *env, jclass cl, jint IpAd
 		TimeoutMs);
 
 	if (ReplysReceived == 0) {
-
+#ifdef _DEBUG
 		WCHAR IpErrorMsg[1024];
 		DWORD ErrSize = sizeof(IpErrorMsg);
 		GetIpErrorString(ReplyBuffer.reply.Status, IpErrorMsg, &ErrSize);
-
+#endif
 		DWORD replyLastError = GetLastError();
 		return replyLastError;
 	}
 
 	const ULONG IpStatus = ReplyBuffer.reply.Status;
-
-	//
-	// free up resources
-	//
-	IcmpCloseHandle(hIcmpFile);
 
 	return IpStatus;
 }
--- src/at/spindi/WinPing.java
+++ src/at/spindi/WinPing.java
@@ -7,8 +7,10 @@ public class WinPing {
 	static {
 	      System.loadLibrary("WinPingJni"); // Load native library at runtime
 	                                   // hello.dll (Windows) or libhello.so (Unixes)
-	   }
+	}
 	
+	private static native int native_WinPing_Startup();
+	private static native int native_WinPing_Cleanup();
 	private static native int native_icmp_WinPing4(int IpAdress, int timeout); 
 	
 	public static int ping4(Inet4Address v4ToPing, int timeoutMs) {
