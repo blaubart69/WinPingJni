6e60db1d41377b8d41e55f5ee00c5591b380c73b
bee <bernhard.spindler.75@gmail.com>
2019-08-04 05:54:44 +0200

    optical changes

--- jni/WinPingJni/Ping4Async.c
+++ jni/WinPingJni/Ping4Async.c
@@ -4,9 +4,7 @@
 // global structure initialized via "startup" call
 extern WIN_PING_GLOBAL* gWinPing;
 
-//DWORD	WINAPI	ThreadProc(LPVOID lpThreadParameter);
-VOID	NTAPI	ApcSendPingAsync(ULONG_PTR Parameter);
-//DWORD			enqueue(PING_CTX* pingCtx);
+//VOID	NTAPI	ApcSendPingAsync(ULONG_PTR Parameter);
 
 // -----------------------------------------------------------------------------
 void jniPingCompletedCallback(IPAddr ip, int LastError, jlong pingStatus, jint roundtrip, jobject globalRefobjConsumer) {
@@ -21,8 +19,8 @@ void jniPingCompletedCallback(IPAddr ip, int LastError, jlong pingStatus, jint r
 	//
 	// calling the "consumer" callback
 	//
-	const jclass	consumerClass = (*APCJniEnv)->GetObjectClass	(APCJniEnv, globalRefobjConsumer);
-	const jmethodID acceptMethod  = (*APCJniEnv)->GetMethodID		(APCJniEnv, consumerClass, "accept", "(Ljava/lang/Object;)V");
+	const jclass	consumerClass = (*APCJniEnv)->GetObjectClass(APCJniEnv, globalRefobjConsumer);
+	const jmethodID acceptMethod = (*APCJniEnv)->GetMethodID(APCJniEnv, consumerClass, "accept", "(Ljava/lang/Object;)V");
 	//
 	// call the callback consumer/accept with the WinPingResult object
 	//
@@ -32,6 +30,84 @@ void jniPingCompletedCallback(IPAddr ip, int LastError, jlong pingStatus, jint r
 	//
 	(*APCJniEnv)->DeleteGlobalRef(APCJniEnv, globalRefobjConsumer);
 }
+// -----------------------------------------------------------------------------
+VOID NTAPI ApcOnPingCompleted(IN PVOID ApcContext, IN PIO_STATUS_BLOCK IoStatusBlock, IN ULONG Reserved) {
+// -----------------------------------------------------------------------------
+	PING4_CTX* pingCtx = (PING4_CTX*)ApcContext;
+
+	DWORD replies = IcmpParseReplies(&pingCtx->icmpReply, sizeof(MY_ICMP_REPLY));
+
+	if (replies > 0) {
+		jniPingCompletedCallback(
+			pingCtx->ip
+			, 0
+			, (jlong)(pingCtx->icmpReply.reply.Status)
+			, (jint)(pingCtx->icmpReply.reply.RoundTripTime)
+			, pingCtx->globalRefobjConsumer);
+	}
+	else {
+		// The IcmpParseReplies function returns the number of ICMP responses found on success. 
+		// The function returns zero on error. 
+		// Call GetLastError for additional error information.
+		jniPingCompletedCallback(
+			pingCtx->ip
+			, 0							// LastError
+			, (jlong)GetLastError()		// IpStatus
+			, (jint)-1					// roundtrip
+			, pingCtx->globalRefobjConsumer);
+	}
+
+	HeapFree(GetProcessHeap(), 0, pingCtx);
+}
+// -----------------------------------------------------------------------------
+VOID NTAPI ApcSendPingAsync(ULONG_PTR Parameter) {
+	// -----------------------------------------------------------------------------
+	PING4_CTX* pingCtx = (PING4_CTX*)Parameter;
+	//
+	// we are now on the "APC-ping-thread"
+	// let's send an async ping...
+	//
+	const DWORD rcSendecho = IcmpSendEcho2(
+		gWinPing->hIcmpFile,
+		NULL,						// event
+		ApcOnPingCompleted,
+		pingCtx,					// Apc context
+		pingCtx->ip,
+		(LPVOID) & (pingCtx->icmpReply.data),
+		sizeof(MY_DATA),
+		NULL,						//PIP_OPTION_INFORMATION
+		(LPVOID) & (pingCtx->icmpReply.reply),
+		sizeof(MY_ICMP_REPLY),
+		pingCtx->timeoutMs);
+	/*
+		When called asynchronously, the IcmpSendEcho2 function returns ERROR_IO_PENDING to indicate the operation is in progress.
+		The results can be retrieved later when the event specified in the Event parameter signals or the callback function
+		in the ApcRoutine parameter is called.
+		If the return value is zero, call GetLastError for extended error information.
+		If the function fails, the extended error code returned by GetLastError can be one of the following values.
+		...
+		ERROR_IO_PENDING
+		...
+
+		--> this all means, WHEN we get an RC != ERROR_IO_PENDING
+			THEN the GetLastError() could also be ERROR_IO_PENDING and does mean NO_ERROR!!! (grrrr!)
+	*/
+	if (rcSendecho != ERROR_IO_PENDING) {
+		// MAYBE an error. Check again.
+		const int send2LastError = GetLastError();
+
+		//  check again if IO_PENDING
+		if (send2LastError != ERROR_IO_PENDING) {
+			//
+			// some error occured. APC will not be called.
+			// clean up here
+			//
+			jniPingCompletedCallback(pingCtx->ip, send2LastError, -1, 0, pingCtx->globalRefobjConsumer);
+			HeapFree(GetProcessHeap(), 0, pingCtx);
+		}
+	}
+}
+
 /*
 * Class:     at_spindi_WinPing
 * Method:    native_icmp_WinPing4Async
@@ -61,81 +137,4 @@ JNIEXPORT jint JNICALL Java_at_spindi_WinPing_native_1icmp_1WinPing4Async
 	}
 
 	return rc;
-}
-
-// -----------------------------------------------------------------------------
-VOID NTAPI ApcOnPingCompleted(IN PVOID ApcContext, IN PIO_STATUS_BLOCK IoStatusBlock, IN ULONG Reserved) {
-// -----------------------------------------------------------------------------
-	PING4_CTX* pingCtx = (PING4_CTX*)ApcContext;
-
-	DWORD replies = IcmpParseReplies(&pingCtx->icmpReply, sizeof(MY_ICMP_REPLY));
-
-	if (replies > 0) {
-		jniPingCompletedCallback(
-			pingCtx->ip
-			, 0
-			, (jlong)(pingCtx->icmpReply.reply.Status)
-			, (jint)(pingCtx->icmpReply.reply.RoundTripTime)
-			, pingCtx->globalRefobjConsumer);
-	}
-	else {
-		// The IcmpParseReplies function returns the number of ICMP responses found on success. The function returns zero on error. 
-		// Call GetLastError for additional error information.
-		jniPingCompletedCallback(
-			pingCtx->ip
-			, 0
-			, (jlong)GetLastError()
-			, (jint)-1
-			, pingCtx->globalRefobjConsumer);
-	}
-
-	HeapFree(GetProcessHeap(), 0, pingCtx);
-}
-// -----------------------------------------------------------------------------
-VOID NTAPI ApcSendPingAsync(ULONG_PTR Parameter) {
-// -----------------------------------------------------------------------------
-	PING4_CTX* pingCtx = (PING4_CTX*)Parameter;
-	//
-	// we are now within the "ping thread"
-	// let's send an async ping...
-	//
-	const DWORD rcSendecho = IcmpSendEcho2(
-		gWinPing->hIcmpFile,		
-		NULL,						// event
-		ApcOnPingCompleted,
-		pingCtx,					// Apc context
-		pingCtx->ip,
-		(LPVOID)&(pingCtx->icmpReply.data),
-		sizeof(MY_DATA),
-		NULL,						//PIP_OPTION_INFORMATION
-		(LPVOID)&(pingCtx->icmpReply.reply),
-		sizeof(MY_ICMP_REPLY),
-		pingCtx->timeoutMs);
-	/*
-		When called asynchronously, the IcmpSendEcho2 function returns ERROR_IO_PENDING to indicate the operation is in progress. 
-		The results can be retrieved later when the event specified in the Event parameter signals or the callback function 
-		in the ApcRoutine parameter is called.
-		If the return value is zero, call GetLastError for extended error information.
-		If the function fails, the extended error code returned by GetLastError can be one of the following values.
-		...
-		ERROR_IO_PENDING
-		...
-
-		--> this all means, WHEN we get an RC != ERROR_IO_PENDING
-			THEN the GetLastError() could also be ERROR_IO_PENDING and does mean NO_ERROR!!! (grrrr!)
-	*/
-	if (rcSendecho != ERROR_IO_PENDING) {
-		// MAYBE an error. Check again.
-		const int send2LastError = GetLastError();
-
-		//  check again if IO_PENDING
-		if (send2LastError != ERROR_IO_PENDING) {
-			//
-			// some error occured. APC will not be called.
-			// clean up here
-			//
-			jniPingCompletedCallback(pingCtx->ip, send2LastError, -1, 0, pingCtx->globalRefobjConsumer);
-			HeapFree(GetProcessHeap(), 0, pingCtx);
-		}
-	}
 }
